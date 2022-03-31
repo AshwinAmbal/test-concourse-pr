85efcd3dda732d1e3edf143e69946f542cb62a16
AshwinAmbal <ashwinambal96@gmail.com>
2022-03-30 18:56:04 -0700

    Add tests to branch

--- tests/default_test.py
+++ tests/default_test.py
@@ -5,6 +5,12 @@ def capital_case(x):
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
 
+
 def test_name():
     name = 'Ashwin'
     assert name == 'Ashwin'
+
+
+def test_job_unequal():
+    job = 'Postman'
+    assert job != 'Engineer'
